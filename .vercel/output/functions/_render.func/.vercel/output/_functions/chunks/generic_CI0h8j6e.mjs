a6ad888b2ed2daa2c9271514a29baf76da327fdd
Sxkin21 <shahalal1@outlook.com>
2024-04-17 22:19:51 +0100

    initial commit

---
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/generic_CI0h8j6e.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./pages/generic_DZmoUKNe.mjs');
+
+export { page };
---
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/register_D-SIW7Lw.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./pages/register_PiPKo7-v.mjs');
+
+export { page };
