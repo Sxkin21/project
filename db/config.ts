a25c2e832be19225f6f64e87678d336512f9faff
Sxkin21 <shahalal1@outlook.com>
2024-04-20 20:37:48 +0100

    modified:   astro.config.mjs 	modified:   db/config.ts 	modified:   db/seed.ts 	modified:   package.json 	modified:   src/pages/register.astro

--- db/config.ts
+++ db/config.ts
@@ -2,8 +2,6 @@ import { defineDb, defineTable, column } from 'astro:db';
 
 
 
-
-<<<<<<< HEAD
 const users= defineTable({
   columns: {
     email: column.text(),
@@ -16,9 +14,4 @@ const users= defineTable({
 // https://astro.build/db/config
 export default defineDb({
   tables: {users}
-=======
-// https://astro.build/db/config
-export default defineDb({
-  tables: {}
->>>>>>> 0300e722a1287109d9083dd4060cb2e9fdbeb789
 });
--- db/seed.ts
+++ db/seed.ts
@@ -2,9 +2,4 @@ import { db } from 'astro:db';
 
 // https://astro.build/db/seed
 export default async function seed() {
-<<<<<<< HEAD
-
-=======
-	// TODO
->>>>>>> 0300e722a1287109d9083dd4060cb2e9fdbeb789
 }
