739fe17381c3a433466cc31f4f0e25877b3bbe8f
Sxkin21 <shahalal1@outlook.com>
2024-04-20 20:43:43 +0100

    modified:   .astro/db-types.d.ts 	new file:   .netlify/functions-internal/ssr/_noop-middleware.mjs 	new file:   .netlify/functions-internal/ssr/chunks/astro/assets-service_C4VePfZt.mjs 	new file:   .netlify/functions-internal/ssr/chunks/astro_Ce5QnvEV.mjs 	new file:   .netlify/functions-internal/ssr/chunks/generic_DedqcgJn.mjs 	new file:   .netlify/functions-internal/ssr/chunks/pages/generic_Bo6bRCNU.mjs 	new file:   .netlify/functions-internal/ssr/chunks/pages/register_MzKjfqx9.mjs 	new file:   .netlify/functions-internal/ssr/chunks/register_BTz0pLYG.mjs 	new file:   .netlify/functions-internal/ssr/entry.mjs 	new file:   .netlify/functions-internal/ssr/manifest_WRMbRDhg.mjs 	new file:   .netlify/functions-internal/ssr/renderers.mjs 	new file:   .netlify/functions-internal/ssr/ssr.mjs 	new file:   dist/_astro/register.-kB7tUh_.css 	new file:   dist/robots.txt

---
+++ .netlify/functions-internal/ssr/chunks/generic_DedqcgJn.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./pages/generic_Bo6bRCNU.mjs');
+
+export { page };
---
+++ .netlify/functions-internal/ssr/chunks/pages/register_MzKjfqx9.mjs
@@ -0,0 +1,33 @@
+/* empty css                             */
+import { e as createAstro, f as createComponent, r as renderTemplate, i as renderHead } from '../astro_Ce5QnvEV.mjs';
+import 'kleur/colors';
+import 'html-escaper';
+import 'clsx';
+import { createRemoteDatabaseClient, asDrizzleTable } from '@astrojs/db/runtime';
+import { isNotNull } from '@astrojs/db/dist/runtime/virtual.js';
+
+const db = await createRemoteDatabaseClient(process.env.ASTRO_STUDIO_APP_TOKEN, {"BASE_URL": "/", "MODE": "production", "DEV": false, "PROD": true, "SSR": true, "SITE": undefined, "ASSETS_PREFIX": undefined}.ASTRO_STUDIO_REMOTE_DB_URL ?? "https://db.services.astro.build");
+const users = asDrizzleTable("users", { "columns": { "email": { "type": "text", "schema": { "unique": false, "deprecated": false, "name": "email", "collection": "users", "primaryKey": false, "optional": false } }, "password": { "type": "text", "schema": { "unique": false, "deprecated": false, "name": "password", "collection": "users", "primaryKey": false, "optional": false } } }, "deprecated": false, "indexes": {} }, false);
+
+const $$Astro = createAstro();
+const $$Register = createComponent(async ($$result, $$props, $$slots) => {
+  const Astro2 = $$result.createAstro($$Astro, $$props, $$slots);
+  Astro2.self = $$Register;
+  if (Astro2.request.method === "POST") {
+    const formData = await Astro2.request.formData();
+    formData.get("email");
+    formData.get("password");
+    const emailData = await db.select().from(users).where(isNotNull(users.email));
+    console.log(emailData);
+  }
+  return renderTemplate`<html lang="en"> <head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Document</title><link rel="stylesheet" href="/src/output.css">${renderHead()}</head> <body class="min-h-screen grid place-content-center radial-blue"> <form class="card w-96 bg-base-100 shadow-xl" method="POST"> <div class="card-body"> <h2 class="card-title mb-5">Sign Up!</h2> <label class="input input-bordered flex items-center gap-5 mb-3 " for="email">
+Email
+<input type="email" id="email " name="email" class="grow" placeholder="youremail@domain.com" required> </label> <label class="input input-bordered flex items-center gap-6" for="password">
+Password
+<input type="password" id="password" name="password" class="grow" placeholder="Password123" required> </label> <div class="card-actions justify-end text-center mx-auto mt-6"> <button type="submit" class="btn btn-primary left-1/2 w-28 font-extrabold">Create Account</button> </div> </div> </form> </body></html>`;
+}, "C:/Users/moals/Downloads/project/src/pages/register.astro", void 0);
+
+const $$file = "C:/Users/moals/Downloads/project/src/pages/register.astro";
+const $$url = "/register";
+
+export { $$Register as default, $$file as file, $$url as url };
