223aaf15bc93e7d7374e74bbfe9b85f33c471618
Sxkin21 <shahalal1@outlook.com>
2024-04-20 19:36:43 +0100

    modified:   astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import db from "@astrojs/db";
+import daisyui from 'daisyui';
 
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), db()],
+  integrations: [tailwind(), db(), daisyui()],
   output: "server",
   adapter: vercel()
 });
