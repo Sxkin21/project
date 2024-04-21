9c2894f25fbeccaff0f8cd718b42ec25dcd09a68
Sxkin21 <shahalal1@outlook.com>
2024-04-21 18:42:27 +0100

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'project/main'.
    
     Changes to be committed:
    	modified:   .astro/link
    	deleted:    .github/workflows/_studio.yml
    	deleted:    .netlify/functions-internal/ssr/_noop-middleware.mjs
    	deleted:    .netlify/functions-internal/ssr/chunks/astro/assets-service_C4VePfZt.mjs
    	deleted:    .netlify/functions-internal/ssr/chunks/astro_Ce5QnvEV.mjs
    	deleted:    .netlify/functions-internal/ssr/chunks/generic_DedqcgJn.mjs
    	deleted:    .netlify/functions-internal/ssr/chunks/pages/generic_Bo6bRCNU.mjs
    	deleted:    .netlify/functions-internal/ssr/chunks/pages/register_MzKjfqx9.mjs
    	deleted:    .netlify/functions-internal/ssr/chunks/register_BTz0pLYG.mjs
    	deleted:    .netlify/functions-internal/ssr/entry.mjs
    	deleted:    .netlify/functions-internal/ssr/manifest_WRMbRDhg.mjs
    	deleted:    .netlify/functions-internal/ssr/renderers.mjs
    	deleted:    .netlify/functions-internal/ssr/ssr.mjs
    	modified:   db/seed.ts
    	modified:   package.json
    	modified:   src/pages/register.astro

--- .netlify/functions-internal/ssr/chunks/generic_DedqcgJn.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/generic_Bo6bRCNU.mjs');
-
-export { page };
--- .netlify/functions-internal/ssr/chunks/register_BTz0pLYG.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/register_MzKjfqx9.mjs');
-
-export { page };
