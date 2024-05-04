896c2466d9e9aa75fb92cf0bfe866d7e3a86b52a
Sxkin21 <shahalal1@outlook.com>
2024-05-04 14:28:23 +0100

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'project/main'.
    
     Changes to be committed:
    	modified:   .astro/content.db astro build
    	modified:   .env
    	modified:   astro.config.mjs
    	new file:   auth_config.json
    	modified:   package-lock.json
    	modified:   package.json
    	new file:   src/firebase/client.ts
    	new file:   src/firebase/server.ts
    	new file:   src/pages/api/auth/signin.ts
    	modified:   src/pages/main.astro
    	modified:   tsconfig.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import db from "@astrojs/db";
-
 import netlify from "@astrojs/netlify";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), db()],
+  integrations: [tailwind(), db(), react()],
   output: "server",
-  adapter: netlify(),
-
-  
- 
+  adapter: netlify()
 });
---
+++ src/firebase/client.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAv4dfu6Pd0V63q0qsZaBdbhsGfo8nsrGg",
+  authDomain: "my-project-3b4b5.firebaseapp.com",
+  projectId: "my-project-3b4b5",
+  storageBucket: "my-project-3b4b5.appspot.com",
+  messagingSenderId: "171373074370",
+  appId: "1:171373074370:web:ef63f4f187fca2dce5e85b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
